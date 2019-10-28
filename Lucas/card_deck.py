035120e636019335ed77542e9fea850f13e6995f
Lucas <circuitsacul@gmail.com>
2019-10-28 05:18:57 -0400

    Modified Lucas/card_deck.py

--- Lucas/card_deck.py
+++ Lucas/card_deck.py
@@ -2,7 +2,7 @@
 classes = ["Hearts", "Diamonds", "Spades", "Clubs"]
 values = ["2", "3", "4", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 
-def make_deck():
+def make_deck(): #Something is wrong with this function
     deck = []
     for i in range(0, 2):
 	deck.append(Card("joker", "joker"))
@@ -13,27 +13,19 @@ def make_deck():
 
 
 def join_decks(deck1, deck2):
-    decks = [deck1, deck2]
+    deck_len = len(deck1) + len(deck2)
     joined_deck = []
-    card_count = len(deck1) + len(deck2)
-    current_deck = 1
-    for i in range(0, card_count):
-        if current_deck == 0:
-            current_deck = 1
-        else:
-            current_deck = 0
-        try:
-            joined_deck.append(decks[current_deck][-1])
-            decks[current_deck].pop(-1)
-        except:
-            pass
+    while len(deck1) > 0 or len(deck2) > 0:
+        for deck in [deck1, deck2]:
+            if len(deck) > 0:
+                joined_deck.append(deck[0])
+                deck.pop(0)
     return joined_deck
-            
 
 
 class Card_deck:
     def __init__(self):
-    	self.deck = make_deck()
+        self.deck = range(0, 52)
     def split_deck(self, split_point):
     	return self.deck[:split_point], self.deck[split_point:]
     def __repr__(self):
@@ -47,14 +39,15 @@ class Card:
     def __repr__(self):
     	return str(self.value + " of " + self.card_class)
 
-card_deck = Card_deck()
-deck1, deck2 = card_deck.split_deck(int(raw_input("Where would you like to split the deck?")))
-
-print "Deck 1"
-print deck1
-print " "
-print "Deck 2"
-print deck2
-print " "
-print "Joined Deck"
-print join_decks(deck1, deck2)
+if __name__ == "__main__":
+    print "This is a demo"
+    card_deck = Card_deck()
+    deck1, deck2 = card_deck.split_deck(int(raw_input("Where would you like to split the deck?")))
+    print "Deck 1"
+    print deck1
+    print " "
+    print "Deck 2"
+    print deck2
+    print " "
+    print "Joined Deck"
+    print join_decks(deck1, deck2)
