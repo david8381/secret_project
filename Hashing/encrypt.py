65a74b4d93413f4a1da2498ee0a4a80e9e3ae643
Lucas Daniels <circuitsacul@gmail.com>
2019-12-09 20:06:52 -0500

    Added final working version of encrypt.py

--- Hashing/encrypt.py
+++ Hashing/encrypt.py
@@ -1,60 +1,52 @@
-from Crypto.Cipher import AES
-from base64 import b64encode, b64decode
-import make_key
+import base64
+import os
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet
 
 
-class Crypt:
-
-    def __init__(self, salt='SlTKeYOpHygTYkP3'):
-        self.salt = salt
-        self.enc_dec_method = 'utf-8'
-
-    def encrypt(self, str_to_enc, str_key):
-        str_key = make_key.convert(str_key)
-        try:
-            aes_obj = AES.new(str_key, AES.MODE_CFB, self.salt)
-            hx_enc = aes_obj.encrypt(str_to_enc)
-            mret = b64encode(hx_enc).decode(self.enc_dec_method)
-            return mret
-        except ValueError as value_error:
-            if value_error.args[0] == 'IV must be 16 bytes long':
-                raise ValueError('Encryption Error: SALT must be 16 characters long')
-            elif value_error.args[0] == 'AES key must be either 16, 24, or 32 bytes long':
-                raise ValueError('Encryption Error: Encryption key must be less than 32 characters long')
-            else:
-                raise ValueError(value_error)
-
-    def decrypt(self, enc_str, str_key):
-        str_key = make_key.convert(str_key)
-        try:
-            aes_obj = AES.new(str_key, AES.MODE_CFB, self.salt)
-            str_tmp = b64decode(enc_str.encode(self.enc_dec_method))
-            str_dec = aes_obj.decrypt(str_tmp)
-            mret = str_dec.decode(self.enc_dec_method)
-            return mret
-        except ValueError as value_error:
-            if value_error.args[0] == 'IV must be 16 bytes long':
-                raise ValueError('Decryption Error: SALT must be 16 characters long')
-            elif value_error.args[0] == 'AES key must be either 16, 24, or 32 bytes long':
-                raise ValueError('Decryption Error: Encryption key must be less than 32 characters long')
-            else:
-                raise ValueError(value_error)
+def generate_key(password_provided):
+    password = password_provided.encode() # Convert to type bytes
+    salt = b'salt_' # CHANGE THIS - recommend using a key from os.urandom(16), must be of type bytes
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=100000,
+        backend=default_backend()
+    )
+    return base64.urlsafe_b64encode(kdf.derive(password)) # Can only use kdf once
 
 
-def menu():
-    print("1: Encrypt a file")
-    print("2: Decrypt a file")
-    value = input("> ")
-    if value in ['1', '2']:
-        return value
-    else:
-        print("Invalid option")
-        return menu()
+def encrypt(text, key):
+    text = text.encode()
+    f = Fernet(key)
+    encrypted = f.encrypt(text)
+    return encrypted
 
 
-if __name__ == "__main__":
-    value = menu()
-    if value == '1':
-        encrypt_file()
-    elif value == '2':
-        decrypt_file()
+def decrypt(encrypted_text, key):
+    f = Fernet(key)
+    to_decrypt = encrypted_text
+    decrypted = f.decrypt(to_decrypt)
+    return decrypted
+
+
+if __name__ == '__main__':
+    password = input("Password> ")
+    text = input("Text to encrypt> ")
+    key = generate_key(password)
+
+    encrypted_text = encrypt(text, key)
+    decrypted_text = decrypt(encrypted_text, key)
+
+    print("\n\n")
+    print("Password: " + password)
+    print("Key from password: " + key.decode())
+    print("\nText you entered:\n" + text)
+    print("\nEncrypted text:\n" + encrypted_text.decode())
+    print("\nAfter decrypting encrypted text:\n" + decrypted_text.decode())
+    print("\n")
+    print("text == decrypted_text: " + str(str(text) == str(decrypted_text.decode())))
+    print("\n\n")
