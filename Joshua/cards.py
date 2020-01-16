287290a7299a8af7a28e7990594d68d38b46b98a
Joshua <yusamac205@gmail.com>
2020-01-16 10:34:41 -0500

    Oops, forgot to push this. Adding my homework

--- Joshua/cards.py
+++ Joshua/cards.py
@@ -1,11 +1,30 @@
+import random
+
 class Deck:
     def __init__(self, cardlist):
-        self.cards = cardlist 
+        self.cards = range(1, cardlist+1)
     def split(self, position):
-        deck1 = Deck(self.cards[0:position])
-        deck2 = Deck(self.cards[position:])
+        deck1 = self.cards[:position]
+        deck2 = self.cards[position:]
         return deck1, deck2
     def __repr__(self):
         return str(self.cards)
-    def connect(self, deck1, deck2):
+    def connect(self, where):
+       deck1, deck2 = self.split(where)
+       jdeck = []
+       while len(deck1) + len(deck2) > 0:
+            if len(deck1) > 0 and len(deck2) > 0:
+                cdeck = random.randint(0,1)
+            elif len(deck1) > 0:
+                cdeck = 0
+            else:
+                cdeck = 1
+            if cdeck == 0:
+                jdeck.append(deck1[0])
+                deck1.pop(0)
+            else:
+                jdeck.append(deck2[0])
+                deck2.pop(0)
+       return jdeck
+
 
