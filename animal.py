0104c0d434d1a4b4560c7258e293d36246757ba9
David Daniels <david.daniels@spensatech.com>
2020-01-30 18:18:50 -0500

    add deck test

--- animal.py
+++ animal.py
@@ -5,12 +5,21 @@ class Animal(object):
         self.sound = sound
 
     def make_sound(self):
-        print(self.sound)
+        print(self.name + ' ' + self.sound)
 
     def get_name(self):
         return self.name
+
+    def replicate_me(self, new_name):
+        new_animal = Animal(new_name, self.sound)
+        return new_animal
 
 if __name__ == '__main__':
 
     dog = Animal('Fido', 'Bark')
     dog.make_sound()
+
+    new_dog = dog.replicate_me('Joe')
+    new_dog.make_sound()
+
+
