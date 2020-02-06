ca5fa2ceb1f6d4852b03ace62d1a1f3bd8d9c52f
Joshua <yusamac205@gmail.com>
2020-02-06 16:44:20 -0500

    Oops, forgot to push again

--- Joshua/cards.py
+++ Joshua/cards.py
@@ -5,14 +5,18 @@ class Deck:
         if type(cardlist) == int:
             self.cards = list(range(1, cardlist+1))
         elif type(cardlist) == list:
-            self.cards = cardlist
-    def split(self, position):
+            self.cards = list(cardlist)
+    def split_deck(self, position):
         deck1 = Deck(self.cards[:position])
         deck2 = Deck(self.cards[position:])
         return deck1, deck2
     def __repr__(self):
         return str(self.cards)
-    def connect(self, deck2):
+    def __len__(self):
+        return len(self.cards)
+    def get_card_list(self):
+        return self.cards
+    def combine_decks(self, deck2):
        deck1 = self.cards
        deck2 = deck2.cards
        jdeck = []
@@ -38,5 +42,23 @@ class Deck:
             d = d1.connect(d2)
             shuffled = shuffled + 1
        return d
+def combine_decks(self, deck2):
+    deck1 = self.cards
+    deck2 = deck2.cards
+    jdeck = []
+    while len(deck1) + len(deck2) > 0:
+        if len(deck1) > 0 and len(deck2) > 0:
+            cdeck = random.randint(0,1)
+        elif len(deck1) > 0:
+            cdeck = 0
+        else:
+            cdeck = 1
+        if cdeck == 0:
+            jdeck.append(deck1[0])
+            deck1.pop(0)
+        else:
+            jdeck.append(deck2[0])
+            deck2.pop(0)
+    return Deck(jdeck)
 
 
